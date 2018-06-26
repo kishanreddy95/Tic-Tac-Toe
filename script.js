0b6a7d852d584303d84ad8c0d5548e80c67ef142
Kishan Reddy <kishanreddy95@gmail.com>
2018-06-26 17:31:42 +0530

    added userMoves

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 var ticTacToe = {
-    values: ['-','-','-','-','-','-','-','-','-']            
+    values: ['-','-','-','-','-','-','-','-','-'],
+    userChance: true          
 }
 
 var controller = {
-    userMove: function() {
-        document.innerHTML = "X";          
+    whichUser: function() {
+        ticTacToe.userChance = !ticTacToe.userChance;
+    },
+    userMove: function(id) {
+       var item = document.getElementById(id);
+       if(item.innerHTML == "") {
+            if(ticTacToe.userChance == true) {
+                item.innerHTML = "X";
+                this.whichUser();
+                             
+            } else {
+                item.innerHTML = "Y";
+                this.whichUser();
+                
+            }    
+        }    
+    },
+    userWinner: function() {
+        var arr = ticTacToe.values;
+        for(var i = 0; i < arr; i++) {
+
+        }
     }
 }
